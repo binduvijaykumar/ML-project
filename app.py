260b2b43881c8540bbed1b59741fb8810a310a4c
binduvijaykumar <bindusv1916@gmail.com>
2026-04-23 22:03:04 +0530

    done

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load("model.pkl")
+st.title("House Price Prediction App")
+
+st.divider()
+
+st.write("This app uses machine learning for predicting house price with given features of the house. For using this app you can enter the inputs this UI and then use predict botton.")
+
+st.divider()
+
+bedrooms = st.number_input("Number of bedrooms", min_value = 0, value = 0)
+bathrooms= st.number_input("Number of bathrooms", min_value = 0, value = 0)
+livingarea = st.number_input("Living area ", min_value = 0, value = 2000)
+condition = st.number_input("Condition", min_value = 0, value = 3)
+numberofschools = st.number_input("Number of schools nearby", min_value = 0, value = 3)
+
+
+st.divider()
+X = [[bedrooms,bathrooms,livingarea,condition,numberofschools]]
+
+predictbutton = st.button("Predict!")
+
+if predictbutton:
+
+    st.balloons()
+
+    X_array = np.array(X)
+
+    prediction = model.predict(X_array)
+
+    st.write(f"Price prediction is {prediction[0]:,.2f}")
+
+else:
+    st.write("Please use predict button after entering values")
+    
+
+
+
+
+
+
+
+
+
+
+#Order of X ['number of bedrooms', 'number of bathrooms', 'living area',
+       #'condition of the house', 'Number of schools nearby']
